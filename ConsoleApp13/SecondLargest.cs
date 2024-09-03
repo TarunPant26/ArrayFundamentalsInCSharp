c25ca9d40fef91912cc969fca5e59974a9f80dfd
Tarun Pant <tarunpant26@hotmail.com>
2024-09-03 09:38:16 +0530

    second largest array with test cases added

---
+++ ConsoleApp13/SecondLargest.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ConsoleApp13
+{
+    public class SecondLargest
+    {
+        public static void Main()
+        {
+            var numbers = new int[] { 2, 5, 7, 10, 12, 14, 15 };
+        }
+
+        public static int FindSecondLargest(int[] numbers)
+        {
+            var largest = int.MinValue;
+            var seconLargest = int.MinValue;
+
+            foreach (var num in numbers)
+            {
+                if(num > largest)
+                {
+                    seconLargest = largest;
+                    largest = num;
+                }
+                else if(seconLargest < num && num!=largest)
+                {
+                    seconLargest = num;
+                }
+            }
+
+            return seconLargest == int.MinValue ? -1 : seconLargest;
+        }
+    }
+}
---
+++ Test_Array_Fundamentals/SecondLargestTest.cs
@@ -0,0 +1,20 @@
+using ConsoleApp13;
+using NUnit.Framework;
+
+namespace Test_Array_Fundamentals
+{
+    public class Tests
+    {
+
+        [TestCase(new int[] { 10,1,11,2,4,5,6,7,},10)]
+        [TestCase(new int[] { 12, 11, 11, 2, 4, 5, 6, 7, }, 11)]
+        [TestCase(new int[] { 10,10,10,10 }, -1)]
+        public void SecondLargest__WhenCalled_ShouldReturnSecondLargestNumber(int [] input,int output)
+        {
+
+            var result = SecondLargest.FindSecondLargest(input);
+
+            Assert.AreEqual(output, result);
+        }
+    }
+}
